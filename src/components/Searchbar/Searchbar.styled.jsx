a15b5a423b4ea1d272d5bca3517d13d59d9406c2
Андрій <122921284+AndrijAyzy@users.noreply.github.com>
2024-02-09 12:57:44 +0200

    dfd

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -14,7 +14,21 @@ export const Header = styled.header`
   padding-top: 12px;
   padding-bottom: 12px;
   color: #fff;
-  background-color: #f29e0c;
+  background: linear-gradient(149deg, rgba(24,187,156,1) 0%, rgba(106,57,175,1) 42%, rgba(187,24,148,1) 72%, rgba(115,53,134,1) 100%);
+    animation: gradient 10s infinite linear; // 10s 
+    background-size: 400%; 
+}
+
+@keyframes gradient {
+    0% {
+        background-position: 80% 0%;
+    }
+    50% {
+        background-position: 20% 100%;
+    }
+    100% {
+        background-position: 80% 0%;
+    }
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
